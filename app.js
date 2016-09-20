c4182c1f81ad310dffce3a1c67672b89d13ef947
LeoTanoue <leo.tanoue@gmail.com>
2016-09-20 00:14:36 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,30 @@
+// define page elements
+var username = document.getElementById('username');
+var message = document.getElementById('message');
+var messages = document.getElementById('messages');
+var form = document.getElementById('form');
+var login = document.getElementById('login');
+
+var messagesRef = firebase.database().ref('messages');
+
+// Retrieve new posts as they are added to Firebase
+messagesRef.on("child_added", function(data) {
+  var newPost = data.val();
+  
+  var msg = document.createElement("div");
+  msg.innerText = newPost.username + ": " + newPost.message;
+  
+  messages.appendChild(msg);
+  console.log(newPost);
+  
+});
+
+form.addEventListener('submit', function(event){
+	event.preventDefault(); //prevent form from doing page reload
+	console.log("form submitterd");
+
+	//append the posted data to the fire database
+	messagesRef.push({'username':username.value,'message':message.value});
+	message.value = ""; //clear the page message
+	message.focus(); //put the cursor in the box for the next message
+});
